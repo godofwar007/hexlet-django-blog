2dc92a1ae9e79a280eac9f46870f42cd0c936845
Peter_007 <hihb3ya@gmail.com>
2025-02-14 16:52:02 +0300

    added app

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from hexlet_django_blog import views
 
 urlpatterns = [
     path('about/', views.about),
     path('', views.index),
+    path('articles/', include('hexlet_django_blog.article.urls')),
+
     path('admin/', admin.site.urls),
+
 ]
